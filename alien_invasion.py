0ab859b5f54f189f431c87e80909e9edd3e44939
David Bonnaud <david@davidbonnaud.com>
2020-11-28 12:51:20 -0800

    added alien ship sprite and made it move

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
   """Overall class to manage game assets and behavior"""
@@ -12,18 +13,53 @@ class AlienInvasion:
     self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    self.ship = Ship(self)
+
   def run_game(self):
     """Start the main loop for the game."""
     while True:
-      # Watch for keyboard and mouse events.
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
+      self._check_events()
+      self.ship.update()
+      self._update_screen()
 
-      # Redraw the screen during each pass through the loop
-      self.screen.fill(self.settings.bg_color)
-      # make the most recently drawn screen visible.
-      pygame.display.flip()
+  def _check_events(self):
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+          # Move the ship to the right.
+          self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+          # Move the ship to the right.
+          self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+          # Move the ship to the right.
+          self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+          # Move the ship to the right.
+          self.ship.moving_down = True
+      elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT:
+          # Move the ship to the right.
+          self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+          # Move the ship to the right.
+          self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+          # Move the ship to the right.
+          self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+          # Move the ship to the right.
+          self.ship.moving_down = False
+
+  def _update_screen(self):
+    self.screen.fill(self.settings.bg_color)
+    self.ship.blitme()
+    # make the most recently drawn screen visible.
+    pygame.display.flip()
+
 
 if __name__ == '__main__':
   ai = AlienInvasion()
