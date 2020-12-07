509479cdb9ce140cfe6675a4734f55267d9fd6da
David Bonnaud <david@davidbonnaud.com>
2020-12-06 20:44:40 -0800

    make ship move at set speed

--- ship.py
+++ ship.py
@@ -6,13 +6,18 @@ class Ship:
   def __init__(self, ai_game):
     """Initialize the ship and set its starting point"""
     self.screen = ai_game.screen
+    self.settings = ai_game.settings
     self.screen_rect = ai_game.screen.get_rect()
+
 
     #Load the ship image and get its rect.
     self.image = pygame.image.load('images/ship.bmp')
     self.rect = self.image.get_rect()
     #start each new ship at the bottom center of the screen
     self.rect.midbottom = self.screen_rect.midbottom
+
+    #store a decimal value for the ships horizontal position
+    self.x = float(self.rect.x)
 
     #Movement flag
     self.moving_right = False
@@ -30,6 +35,9 @@ class Ship:
       self.rect.y -= 1
     if self.moving_down:
       self.rect.y += 1
+
+    #update rect object from self.x
+    self.rect.x = self.x
 
   def blitme(self):
     """Draw the ship at its current location"""
